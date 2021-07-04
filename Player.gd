c5a498216695ae3ce7451943dd9e1adfbdc4396c
Kanmi <kanmiafolabi11@gmail.com>
2021-07-04 20:17:30 +0100

    COMMIT 6// Updated Player and Sword Sprite sheets. Added attack state to both and synced the directions between them

--- Player.gd
+++ Player.gd
@@ -1,25 +1,34 @@
 extends KinematicBody2D
+
+signal is_attacking(attackStatus)
+signal is_facing_right(facing_direction)
+
 #Constants
-const MOVE_SPEED = 200 #pixels per second
+const MOVE_SPEED = 170 #pixels per second
 const JUMP_FORCE = 700 #pixels per second
 const GRAVITY = 40 #pixels per second
 const MAX_FALL_SPEED = 1000 #pixels per second
+
 #importing node references
 onready var sprite = $Sprite
 #Define variables
 var yVelocity = 0
 var facingRight = true
+var attacking = false
 
-func _physics_process(delta): #Handles all the physics of the character movement
+func _physics_process(_delta): #Handles all the physics of the character movement
 	var moveDirection = 0
 	var grounded = is_on_floor()
 	
+	if attacking == false:
 	#Input controller (Horizontal)
-	if Input.is_action_pressed("ui_right"):
-		moveDirection += 1
-	if Input.is_action_pressed("ui_left"):
-		moveDirection -= 1
-	move_and_slide(Vector2(moveDirection * MOVE_SPEED,yVelocity),Vector2(0,-1))
+		if Input.is_action_pressed("ui_right"):
+			moveDirection += 1
+			emit_signal("is_facing_right",facingRight)
+		if Input.is_action_pressed("ui_left") :
+			moveDirection -= 1
+			emit_signal("is_facing_right",facingRight)
+		move_and_slide(Vector2(moveDirection * MOVE_SPEED,yVelocity),Vector2(0,-1))
 	
 	#Input controller (Vertical)
 	yVelocity += GRAVITY
@@ -29,7 +38,13 @@ func _physics_process(delta): #Handles all the physics of the character movement
 		yVelocity = 2
 	if yVelocity > MAX_FALL_SPEED: #Limits the vertical velocity to a maximum fall speed
 		yVelocity = MAX_FALL_SPEED
-		
+	
+	if grounded and Input.is_action_just_pressed("attack"):
+		attacking = true
+		emit_signal("is_attacking",attacking)
+		moveDirection = 0
+		$Sprite.play("attack")
+	
 	#Sprite Flipper
 	if facingRight and moveDirection < 0:
 		flip()
@@ -37,15 +52,26 @@ func _physics_process(delta): #Handles all the physics of the character movement
 		flip()
 		
 	#Animation Handler
-	if grounded:
+	if grounded and attacking == false:
 		if moveDirection == 0:
 			$Sprite.play("idle")
 		else:
 			$Sprite.play("run")
-	else:
+#	else:
+#		$Sprite.play("jump")
+	elif grounded and attacking == true:
+		moveDirection = 0
+		$Sprite.play("attack")
+	elif !grounded:
 		$Sprite.play("jump")
 
 #Sprite Flipper Function
 func flip():
 	facingRight = !facingRight
 	sprite.flip_h = !sprite.flip_h
+
+
+func _on_Sprite_animation_finished():
+	if $Sprite.animation == "attack":
+		attacking = false
+		emit_signal("is_attacking",attacking)
--- Sword.gd
+++ Sword.gd
@@ -1,23 +1,65 @@
 extends KinematicBody2D
 
-onready var player = get_node("res://Player.tscn")
-var target = null
+#var target = null
+onready var sprite = $AnimatedSprite
+var speed = 300
+var distance = 0
+const INITIAL_DISTANCE = 700
 var move = Vector2.ZERO
-var MOVE_SPEED = 3
+const MOVE_SPEED = 3
+var direction = null
+var moveDirection = 0
+var sword_facing_right = true
 
+onready var target = get_node("/root/world/Player")
 
-func _on_Area2D_body_entered(body):
+func _on_Area2D_body_entered(_body):
 	target = null
 
 func _on_Area2D_body_exited(body):
-	if body != self:
-		target = body
+	if body != self :
+		target = get_node("/root/world/Player")
 
 func _physics_process(delta):
-	if target!=null:
-		move = position.direction_to(target.position) *  MOVE_SPEED
+	if target:
+		direction = (target.position - position).normalized()
+		move_and_collide(direction * MOVE_SPEED)
+	var step = moveDirection * speed * delta
+	if moveDirection > 0:
+		if distance > step:
+			move_and_collide(Vector2(step,0))
+		else:
+			distance = 0
+	elif moveDirection < 0:
+		if distance < step:
+			move_and_collide(Vector2(step,0))
+		else:
+			distance = 0
+#	move_and_collide(move)
+	if sword_facing_right and moveDirection < 0:
+		flip()
+	if !sword_facing_right and moveDirection > 0:
+		flip()
+
+
+func _on_Player_is_attacking(attackStatus):
+	if attackStatus == true and target == null:
+		if moveDirection > 0:
+			distance = INITIAL_DISTANCE
+			$AnimatedSprite.play("attack1")
+		else:
+			distance = INITIAL_DISTANCE * moveDirection
+			$AnimatedSprite.play("attack1")
 	else:
-		move = Vector2.ZERO
-	
-#	move = move.normalized()
-	move_and_collide(move)
+		$AnimatedSprite.play("idle")
+		distance = 0
+
+func _on_Player_is_facing_right(facing_direction):
+	if facing_direction == true:
+		moveDirection = 1
+	else:
+		moveDirection = -1
+
+func flip():
+	sword_facing_right = !sword_facing_right
+	sprite.flip_h = !sprite.flip_h
